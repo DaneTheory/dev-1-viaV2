edea8f5077195f2b15d6b25770b212f929aa1d55
Branden Dane <branden@danetheory.com>
2015-05-04 08:09:18 -0400

    init

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$( document ).ready(function() {
+    
+	  
+	  $('.isotope').isotope({
+	    itemSelector: '.mainPhoto',
+	    masonry: {
+	      columnWidth: 100
+	    }
+	  });    
+    
+    
+});
